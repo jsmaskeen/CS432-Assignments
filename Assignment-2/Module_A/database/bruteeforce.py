4cc0b5ad1a896b3aef89b73e8d4d0761aad300ed
jsmaskeen <jsmaskeen@gmail.com>
2026-03-11 15:15:31 +0530

    update bruteforce to store values to offfer a fair ocomparison against bplustree

--- Assignment-2/Module_A/database/bruteeforce.py
+++ Assignment-2/Module_A/database/bruteeforce.py
@@ -1,16 +1,29 @@
 class BruteForceDB:
     def __init__(self):
-        self.data = []
+        self.data = []  # store values also, hence it had (key, value) tuples (to compare it farly with B+ tree)
 
-    def insert(self, key):
-        self.data.append(key)
+    def insert(self, key, value=None):
+        self.data.append((key, value))
 
     def search(self, key):
-        return key in self.data
+        for k, v in self.data:
+            if k == key:
+                return v
+        return None
 
     def delete(self, key):
-        if key in self.data:
-            self.data.remove(key)
+        for i, (k, _) in enumerate(self.data):
+            if k == key:
+                self.data.pop(i)
+                return True
+        return False
+
+    def update(self, key, new_value):
+        for i, (k, _) in enumerate(self.data):
+            if k == key:
+                self.data[i] = (key, new_value)
+                return True
+        return False
 
     def range_query(self, start, end):
-        return [k for k in self.data if start <= k <= end]
+        return [(k, v) for k, v in self.data if start <= k <= end]
