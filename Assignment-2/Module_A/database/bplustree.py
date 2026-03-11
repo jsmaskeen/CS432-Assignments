2fa9e10cdb8ec1583ebfcd372973addb8e5c929e
jsmaskeen <jsmaskeen@gmail.com>
2026-03-12 00:15:19 +0530

    bplustree; implement delete!!! crazy stuff

--- Assignment-2/Module_A/database/bplustree.py
+++ Assignment-2/Module_A/database/bplustree.py
@@ -1,4 +1,5 @@
 from typing import List, Dict, Any, Union, Tuple, Optional, overload, Literal
+from math import ceil
 
 
 class BPlusTreeNode:
@@ -196,19 +197,186 @@ class BPlusTree:
 
         return res
 
+    def delete(self, key: int):
+        min_keys = (
+            ceil(self.degree / 2) - 1
+        )  # minimum keys for non leaf nodes except root
+        cur = self.root
+        nodes_having_key: List[Tuple[BPlusTreeNode, int]] = []  # last one is the leaf
+        while not cur.leaf:
+            loc = len(cur.keys)
+            for i, k in enumerate(cur.keys):
+                if k >= key:
+                    if k == key:
+                        loc = i + 1
+                        nodes_having_key.append((cur, i))
+                    else:
+                        loc = i
+                    break
+            cur = cur.children[loc]
 
-# def delete(self, key):
-#     """
-#     Delete key from the B+ tree.
-#     Handle underflow by borrowing from siblings or merging nodes.
-#     Update the root if it becomes empty.
-#     Return True if deletion succeeded, False otherwise.
-#     """
-#     pass
+        leaf_idx = None
+        for idx, k in enumerate(cur.keys):
+            if k == key:
+                leaf_idx = idx
+                break
 
+        if leaf_idx is None:
+            return False  # key not found
 
-# def _delete(self, node, key):
-#     # Recursive helper for deletion. Handle leaf and internal nodes .
-#     # Ensure all nodes maintain minimum keys after deletion.
-#     pass
+        nodes_having_key.append((cur, leaf_idx))
+        leaf_node = cur
 
+        leaf_node.keys.pop(leaf_idx)
+        leaf_node.values.pop(leaf_idx)
+
+        # if key was present in internal nodes, we neeed to replace it with the new first key of this leaf
+        if len(nodes_having_key) > 1 and len(leaf_node.keys) > 0:
+            internal_key = leaf_node.keys[0]
+            for ancestor, anc_idx in nodes_having_key[:-1]:  # all except the leaf
+                ancestor.keys[anc_idx] = internal_key
+
+        if leaf_node is self.root:
+            return True
+
+        if len(leaf_node.keys) >= min_keys:
+            return True
+
+        self._fix_underflow(leaf_node, min_keys)
+        return True
+
+    def _fix_underflow(self, node: BPlusTreeNode, min_keys: int):
+        # if node is root, the tree height will shrink
+        if node.parent is None:
+            if len(node.keys) == 0 and len(node.children) == 1:
+                self.root = node.children[0]
+                self.root.parent = None
+            return
+
+        parent = node.parent
+        idx_in_parent = parent.children.index(node)
+
+        if node.leaf:
+            left_neigh = (
+                node.prev
+                if (node.prev is not None and node.prev.parent is parent)
+                else None
+            )
+            right_neigh = (
+                node.next
+                if (node.next is not None and node.next.parent is parent)
+                else None
+            )
+        else:
+            # internal nodes aren't in a linked list.
+            left_neigh = (
+                parent.children[idx_in_parent - 1] if idx_in_parent > 0 else None
+            )
+            right_neigh = (
+                parent.children[idx_in_parent + 1]
+                if idx_in_parent < len(parent.children) - 1
+                else None
+            )
+
+        if left_neigh is not None and len(left_neigh.keys) > min_keys:
+            self._take_from_left(node, left_neigh, parent, idx_in_parent)
+            return
+
+        if right_neigh is not None and len(right_neigh.keys) > min_keys:
+            self._take_from_right(node, right_neigh, parent, idx_in_parent)
+            return
+
+        # if we cannot take from either neighbors
+
+        if left_neigh is not None:
+            self._merge_nodes(left_neigh, node, parent, idx_in_parent - 1)
+        elif right_neigh is not None:
+            self._merge_nodes(node, right_neigh, parent, idx_in_parent)
+        else:
+            raise Exception("idk what happened :cries:")
+
+        # parent underflow check
+        if parent.parent is None:  # parent is root
+            if len(parent.keys) == 0:
+                # root has no keys, its first child becomes root
+                self.root = parent.children[0]
+                self.root.parent = None
+        elif len(parent.keys) < min_keys:
+            self._fix_underflow(parent, min_keys)
+
+    def _take_from_left(
+        self,
+        node: BPlusTreeNode,
+        left_neigh: BPlusTreeNode,
+        parent: BPlusTreeNode,
+        idx_in_parent: int,
+    ):
+        # idx_in_parent - 1  =======>>> parent key between left_neigh and node
+
+        if node.leaf:
+            # take last key-value from left neigh, put at start of node
+            borrowed_key = left_neigh.keys.pop(-1)
+            borrowed_val = left_neigh.values.pop(-1)
+            node.keys.insert(0, borrowed_key)
+            node.values.insert(0, borrowed_val)
+            # update parent's middle key to be the new first key of node
+            parent.keys[idx_in_parent - 1] = node.keys[0]
+        else: # non leaf node
+            #  pull middle key down from parent, push last key of left_neigh up
+            node.keys.insert(0, parent.keys[idx_in_parent - 1])
+            parent.keys[idx_in_parent - 1] = left_neigh.keys.pop(-1)
+            # make the last child of left_neigh,  the first child of node
+            child = left_neigh.children.pop(-1)
+            node.children.insert(0, child)
+            child.parent = node
+
+    def _take_from_right(
+        self,
+        node: BPlusTreeNode,
+        right_neigh: BPlusTreeNode,
+        parent: BPlusTreeNode,
+        idx_in_parent: int,
+    ):
+        # idx_in_parent - 1  =======>>> parent key between node and right_neigh
+
+        if node.leaf:
+            # take first key-value from right neihbor, append to node
+            borrowed_key = right_neigh.keys.pop(0)
+            borrowed_val = right_neigh.values.pop(0)
+            node.keys.append(borrowed_key)
+            node.values.append(borrowed_val)
+            # make parent's middle key to be the new first key of right neighbor
+            parent.keys[idx_in_parent] = right_neigh.keys[0]
+        else:
+            # pull middle key down, push first key of right_neigh up
+            node.keys.append(parent.keys[idx_in_parent])
+            parent.keys[idx_in_parent] = right_neigh.keys.pop(0)
+            # make the first child of right_neigh, the last child of node
+            child = right_neigh.children.pop(0)
+            node.children.append(child)
+            child.parent = node
+
+    def _merge_nodes(
+        self,
+        left_node: BPlusTreeNode,
+        right_node: BPlusTreeNode,
+        parent: BPlusTreeNode,
+        middle_idx: int,
+    ):
+        if left_node.leaf:
+            left_node.keys.extend(right_node.keys)
+            left_node.values.extend(right_node.values)
+
+            left_node.next = right_node.next
+            if right_node.next is not None:
+                right_node.next.prev = left_node
+        else:
+            left_node.keys.append(parent.keys[middle_idx])
+            left_node.keys.extend(right_node.keys)
+            left_node.children.extend(right_node.children)
+
+            for child in right_node.children:
+                child.parent = left_node
+
+        parent.keys.pop(middle_idx)
+        parent.children.remove(right_node)
